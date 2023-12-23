894e0a61b61465e0374c6a9a63376df434a6e11f
Sahil Sahu <imsahilsahu11@gmail.com>
2023-12-23 20:31:11 +0530

    sortOptions...

--- src/features/product-list/ProductAPI.js
+++ src/features/product-list/ProductAPI.js
@@ -6,3 +6,19 @@ export function fetchAllProducts() {
     resolve({ data });
   });
 }
+
+export function fetchProductsByFilter(filter) {
+  // filter={"category":"phone"}
+  let queryString = "";
+  for (let key in filter) {
+    queryString += `${key}=${filter[key]}&`;
+  }
+
+  return new Promise(async (resolve) => {
+    const response = await fetch(
+      "http://localhost:8080/products?" + queryString
+    );
+    const data = await response.json();
+    resolve({ data });
+  });
+}
